7ff67553b335fd3edf328c315673834d64c3b28f
Daniel Candia <danfercf@gmail.com>
2017-07-09 00:45:28 -0400

    Add maps downloader

--- installer/install.sh
+++ installer/install.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 sudo cp -R mapcfgs/. /usr/local/ghost/mapcfgs/
+
+file="war3maps060920.zip"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	wget -N http://ftp.blizzard.com/pub/war3/maps/${file}
+fi
+unzip -o war3maps060920.zip -d maps/
 sudo cp -R maps/. /usr/local/ghost/maps/
+
